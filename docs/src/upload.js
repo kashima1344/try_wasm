487490f041b447e6791056587b4b8af97c695577
kashima1344 <kashima1344@gmail.com>
2019-04-13 19:54:27 +0900

    update

--- docs/src/upload.js
+++ docs/src/upload.js
@@ -10,13 +10,43 @@ function preview(ele) {
     var fr = new FileReader();
     fr.onload = function() {
         img.src = fr.result;  // 読み込んだ画像データをsrcにセット
-        document.getElementById('preview_field').appendChild(img);
+        // document.getElementById('preview_field').appendChild(img);
+
+        var ctx = document.getElementById("canvas").getContext("2d");
+        drawImageAspect(ctx, img);
+
     }
     fr.readAsDataURL(file);  // 画像読み込み
 
     // 画像名・MIMEタイプ・ファイルサイズ
     document.getElementById('preview_field').textContent =
-        'file name: ' + file.name + '<br />' +
-        'file type: ' + file.type + '<br />' +
-        'file size: ' + file.size + '<br />';
+        'file name: ' + file.name + ', ' +
+        'file type: ' + file.type + ', ' +
+        'file size: ' + file.size + ;
+}
+
+
+// I referred the following blog. Thank you!
+// https://qiita.com/PG0721/items/599ba2921b8339700fe3
+function drawImageAspect(ctx, img) {
+
+    var canvasAspect = ctx.canvas.width / ctx.canvas.height, // canvasのアスペクト比
+    var imgAspect = img.width / img.height, // 画像のアスペクト比
+    var left, top, width, height;
+
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    if(imgAspect >= canvasAspect) {// 画像が横長
+        left = 0;
+        width = ctx.canvas.width;
+        height = ctx.canvas.width / imgAspect;
+        top = (ctx.canvas.height - height) / 2;
+    } else {// 画像が縦長
+        top = 0;
+        height = ctx.canvas.height;
+        width = ctx.canvas.height * imgAspect;
+        left = (ctx.canvas.width - width) / 2;
+    }
+    ctx.drawImage(img, 0, 0, img.width, img.height, left, top, width, height);
 }
