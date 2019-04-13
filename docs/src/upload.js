781b4c9460a45de74eeaa96520d349aadbf41fd6
kashima1344 <kashima1344@gmail.com>
2019-04-13 20:43:14 +0900

    update

--- docs/src/upload.js
+++ docs/src/upload.js
@@ -6,21 +6,18 @@ function preview(ele) {
     var file = ele.files[0];
     if (!/^image\/(png|jpeg|gif)$/.test(file.type)) return;  // typeプロパティでMIMEタイプを参照
 
-    var img = new Image();
-    var fr = new FileReader();
-    fr.onload = function() {
-        img.src = fr.result;  // 読み込んだ画像データをsrcにセット
-
-        var ctx = document.getElementById("canvas1").getContext("2d");
-        ctx.drawImage(img, 0, 0);
-
-    }
-    fr.readAsDataURL(file);  // 画像読み込み
-
     // 画像名・MIMEタイプ・ファイルサイズ
     document.getElementById('preview_field').textContent =
         'file name: ' + file.name + ', ' +
         'file type: ' + file.type + ', ' +
         'file size: ' + file.size ;
+
+    var ctx = document.getElementById("canvas1").getContext("2d");
+    var img = new Image();
+    img.src = file + "#" + new Date().getTime();
+
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0);
+    }
 }
 
