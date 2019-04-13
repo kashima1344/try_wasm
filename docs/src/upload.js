0bd9dfa576f85df35a4f87f2e63a14517e3867cf
kashima1344 <kashima1344@gmail.com>
2019-04-13 19:40:30 +0900

    update

---
+++ docs/src/upload.js
@@ -0,0 +1,22 @@
+// I referred the following blog. Thank you!
+// https://favril.hatenablog.com/entry/20100506/1273143063
+function preview(ele) {
+    if (!ele.files.length) return;  // ファイル未選択
+    
+    var file = ele.files[0];
+    if (!/^image\/(png|jpeg|gif)$/.test(file.type)) return;  // typeプロパティでMIMEタイプを参照
+
+    var img = document.createElement('img');
+    var fr = new FileReader();
+    fr.onload = function() {
+        img.src = fr.result;  // 読み込んだ画像データをsrcにセット
+        document.getElementById('preview_field').appendChild(img);
+    }
+    fr.readAsDataURL(file);  // 画像読み込み
+
+    // 画像名・MIMEタイプ・ファイルサイズ
+    document.getElementById('preview_field').textContent =
+        'file name: ' + file.name + '<br />' +
+        'file type: ' + file.type + '<br />' +
+        'file size: ' + file.size + '<br />';
+}
