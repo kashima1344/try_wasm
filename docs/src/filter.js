51ac6ee163a306aaac9c5c8dec2d411c92b50ba0
kashima1344 <kashima1344@gmail.com>
2019-10-14 21:55:48 +0900

    Updated

--- docs/src/filter.js
+++ docs/src/filter.js
@@ -1,3 +1,5 @@
+// I referred the following blog. Thank you!
+// https://qiita.com/pentamania/items/62e8a84290d7234c0ad4
 
 var sharedBuffer = null;
 var filteredImageData = null;
@@ -29,9 +31,10 @@ function doFilter() {
     // 共有バッファにコピー
     sharedBuffer.set(currentImage.data);
 
-    // フィルタを処理
     performance.mark("wasm_start2");
+    // フィルタ処理 START
     filter(ptr, width, height);
+    // フィルタ処理 END
     performance.mark("wasm_end2");
 
     // フィルタ処理後の画像をコピー
@@ -44,9 +47,11 @@ function doFilter() {
     performance.measure("wasm2", "wasm_start2", "wasm_end2");
     result2 = performance.getEntriesByName('wasm2');
 
-    document.getElementById('result_wasm').textContent = "success: " + result[0].duration + "(ms); "
-        + result2[0].duration + "(ms)";
+    document.getElementById('result_wasm').textContent = "success: "
+        + "処理全体=" + result[0].duration + "(ms); "
+        + "フィルタ処理のみ=" + result2[0].duration + "(ms)";
 }
+
 
 const COLOR_SUM = 765.0;
 const SEPIA_R = 240.0;
@@ -66,19 +71,21 @@ function doFilterJS() {
 
     performance.mark("js_start2");
 
+    // フィルタ処理 START
     var data = currentImage.data;
     var num = data.length / 4;
 
     for (var i=0; i<num; i++) {
         var p = i * 4;
-        var r = data[p];
+        var r = data[p + 0];
         var g = data[p + 1];
         var b = data[p + 2];
         var avg = (r + g + b) / COLOR_SUM;
-        data[p] = (SEPIA_R * avg);
+        data[p + 0] = (SEPIA_R * avg);
         data[p + 1] = (SEPIA_G * avg);
         data[p + 2] = (SEPIA_B * avg);
     }
+    // フィルタ処理 END
 
     performance.mark("js_end2");
 
@@ -92,7 +99,8 @@ function doFilterJS() {
     performance.measure("js2", "js_start2", "js_end2");
     result2 = performance.getEntriesByName('js2');
 
-    document.getElementById('result_js').textContent = "success:" + result[0].duration + "(ms); "
-        + result2[0].duration + "(ms)";
+    document.getElementById('result_js').textContent = "success: "
+        + "処理全体=" + result[0].duration + "(ms); "
+        + "フィルタ処理のみ=" + result2[0].duration + "(ms)";
 
 }
