60808a5874e4dd36b3057b9652f05e4453fdc7cc
kashima1344 <kashima1344@gmail.com>
2019-10-14 19:06:53 +0900

    optimized

--- docs/src/main.js
+++ docs/src/main.js
@@ -1,10 +1,72 @@
-const { alloc, deallc, filter } = wasm_bindgen;
 
-function runApp() {
-  console.log("Initialized");
+                (function() {
+                    var wasm;
+                    const __exports = {};
+                    
+
+let cachedDecoder = new TextDecoder('utf-8');
+
+let cachegetUint8Memory = null;
+function getUint8Memory() {
+    if (cachegetUint8Memory === null ||
+        cachegetUint8Memory.buffer !== wasm.memory.buffer)
+        cachegetUint8Memory = new Uint8Array(wasm.memory.buffer);
+    return cachegetUint8Memory;
 }
 
-// Load and instantiate the wasm file, and we specify the source of the wasm
-// file here. Once the returned promise is resolved we're ready to go and
-// use our imports.
-wasm_bindgen('../out/main_bg.wasm').then(runApp).catch(console.error);
+function getStringFromWasm(ptr, len) {
+    return cachedDecoder.decode(getUint8Memory().subarray(ptr, ptr + len));
+}
+
+__exports.__wbg_f_alert_alert_n = function(arg0, arg1) {
+    let varg0 = getStringFromWasm(arg0, arg1);
+    alert(varg0);
+};
+
+let cachedEncoder = new TextEncoder('utf-8');
+
+function passStringToWasm(arg) {
+
+    const buf = cachedEncoder.encode(arg);
+    const ptr = wasm.__wbindgen_malloc(buf.length);
+    getUint8Memory().set(buf, ptr);
+    return [ptr, buf.length];
+}
+
+__exports.greet = function(arg0) {
+    const [ptr0, len0] = passStringToWasm(arg0);
+    try {
+        return wasm.greet(ptr0, len0);
+    } finally {
+        wasm.__wbindgen_free(ptr0, len0 * 1);
+    }
+};
+
+__exports.alloc = function(arg0) {
+    return wasm.alloc(arg0);
+};
+
+__exports.dealloc = function(arg0, arg1) {
+    return wasm.dealloc(arg0, arg1);
+};
+
+__exports.filter = function(arg0, arg1, arg2) {
+    return wasm.filter(arg0, arg1, arg2);
+};
+
+__exports.__wbindgen_throw = function(ptr, len) {
+    throw new Error(getStringFromWasm(ptr, len));
+};
+
+                    function init(wasm_path) {
+                        return fetch(wasm_path)
+                            .then(response => response.arrayBuffer())
+                            .then(buffer => WebAssembly.instantiate(buffer, { './rustc_h_s6g8jtwuhg': __exports }))
+                            .then(({instance}) => {
+                                wasm = init.wasm = instance.exports;
+                                return;
+                            });
+                    };
+                    self.wasm_bindgen = Object.assign(init, __exports);
+                })();
+            
